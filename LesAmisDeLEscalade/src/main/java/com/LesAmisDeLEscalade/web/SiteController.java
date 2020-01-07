3ced8a84777eb257636cfd4577a6d09af2cf624e
mustapha <m.touil.mt@gmail.com>
2020-01-07 17:36:55 +0100

    création de la fonctionnalité supprimer un site

--- LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/web/SiteController.java
+++ LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/web/SiteController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.LesAmisDeLEscalade.dao.SiteRepository;
@@ -17,8 +19,14 @@ public class SiteController {
 	
 	@RequestMapping(value="/site")
 	public String site(Model model) {
-		List<Site> site=siteRepository.findAll();
-		model.addAttribute("listSite",site);
-		return "site";
+		List<Site> listSite=siteRepository.findAll();
+		model.addAttribute("listSite", listSite);
+		return "site";		
+		}
+	@GetMapping(value="/site/{id}/supprimer")
+	public String supprimerSite(Model model , @PathVariable(value="id")Long id) {
+		
+		siteRepository.deleteById(id);
+		return "redirect:/site";
 	}
 }
